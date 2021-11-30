6d26dac756fb873d715cf74f73cb71390de7beac
steven tran <steven@stevens-MBP-5.attlocal.net>
2021-11-29 22:24:32 -0800

    post request for createroomview

--- music_player/api/views.py
+++ music_player/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics, serializers, status
+from rest_framework import generics, status
 from .serializers import RoomSerializer, CreateRoomSerializer
 from .models import Room 
 from rest_framework.views import APIView
@@ -21,21 +21,24 @@ class CreateRoomView(APIView):
         if not self.request.session.exists(self.request.session.session_key):
             self.request.session.create()
 
-        serializers = self.serializer_class(data=request.data)
-        if serializers.is_valid():
-            guest_can_pause = serializers.data.get('guest_can_pause')
-            votes_to_skip = serializers.data.get('votes_to_skip')
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            guest_can_pause = serializer.data.get('guest_can_pause')
+            votes_to_skip = serializer.data.get('votes_to_skip')
             host = self.request.session.session_key
             queryset = Room.objects.filter(host=host)
-            if queryset.exist():
+            if queryset.exists():
                 room = queryset[0]
                 room.guest_can_pause = guest_can_pause
                 room.votes_to_skip = votes_to_skip
                 room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+                return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
             else:
                 room = Room(host=host, guest_can_pause=guest_can_pause, votes_to_skip=votes_to_skip)
                 room.save()
+                return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
+        return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
 
-            return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
+            
 
 
