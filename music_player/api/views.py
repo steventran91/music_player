b44b02fd9d8e0a06dcfc4d21e085514e4a92b1b2
steven tran <steven@stevens-MBP-5.attlocal.net>
2021-11-29 18:55:32 -0800

    create or update room

--- music_player/api/views.py
+++ music_player/api/views.py
@@ -18,4 +18,24 @@ class RoomView(generics.ListAPIView):
 class CreateRoomView(APIView):
     serializer_class = CreateRoomSerializer
     def post(self, request, format=None):
-        pass
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        serializers = self.serializer_class(data=request.data)
+        if serializers.is_valid():
+            guest_can_pause = serializers.data.get('guest_can_pause')
+            votes_to_skip = serializers.data.get('votes_to_skip')
+            host = self.request.session.session_key
+            queryset = Room.objects.filter(host=host)
+            if queryset.exist():
+                room = queryset[0]
+                room.guest_can_pause = guest_can_pause
+                room.votes_to_skip = votes_to_skip
+                room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+            else:
+                room = Room(host=host, guest_can_pause=guest_can_pause, votes_to_skip=votes_to_skip)
+                room.save()
+
+            return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
+
+
